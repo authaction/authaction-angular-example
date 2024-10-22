3d9426ad5260cd6a67c6a8574e0406458abeced8
Chandhravadanan Ravichandran <chandhravadanan.ravichandran@agreena.com>
2024-10-22 21:49:54 +0530

    fix: env specific configuration (#3)
    * feat: use custom webpack for dotenv
    
    * fix: env specific configuration
    
    * fix: use system environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const Dotenv = require("dotenv-webpack");
 module.exports = {
   plugins: [
     new Dotenv({
-      path: `./.env`,
+      path: `./.env.${process.env.NODE_ENV}`,
+      systemvars: true,
     }),
   ],
 };
